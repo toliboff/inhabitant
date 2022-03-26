7bae0a6fd607de90e3e6e7e997be2d297517dbb4
Tolibjon <tolibjohn@gmail.com>
2022-03-26 23:01:36 +0500

    Add reducers to store

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,13 +1,14 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunkMiddleware from 'redux-thunk';
-
+import streetReducer from './reducers/streets';
+import houseReducer from './reducers/houses';
+import flatReducer from './reducers/flats';
 
 const reducer = combineReducers({ 
-   streetsReducer,
-  //  housessReducer,
-  //  flatssReducer,
-  //  inhabitantsReducer
+   streetReducer,
+   houseReducer,
+   flatReducer,
   });
 
 const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunkMiddleware)));
